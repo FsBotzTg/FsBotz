998c01af8698b8a0855324d0c38a28dd22ab4067
FsBotz 1 <andromedagalaxynearas@gmail.com>
2024-10-16 10:08:32 +0530

    Add files via upload

---
+++ plugins/Extra/share_text.py
@@ -0,0 +1,20 @@
+import os
+from pyrogram import Client, filters
+from urllib.parse import quote
+from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
+
+@Client.on_message(filters.command(["share_text", "share", "sharetext"]))
+async def share_text(client, message):
+    fi = await client.ask(chat_id = message.from_user.id, text = "Now Send me your text.")
+    if fi and (fi.text or fi.caption):
+        input_text = fi.text or fi.caption
+    else:
+        await fi.reply_text(
+            text=f"**Notice:**\n\n1. Send Any Text Messages.\n2. No Media Support\n\n**Any Question Join Support Chat**",               
+            reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton("Updates Channel", url=f"https://t.me/Fs_Botz")]])
+            )                                                   
+        return
+    await fi.reply_text(
+        text=f"**Here is Your Sharing Text 👇**\n\nhttps://t.me/share/url?url=" + quote(input_text),
+        reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton("♂️ Share", url=f"https://t.me/share/url?url={quote(input_text)}")]])       
+    )
