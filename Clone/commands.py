090fb70e29429690801e000c69e4d9a12eb7c719
Fs Botz <fsbotztg@gmail.com>
2024-12-04 20:56:08 +0530

    Update commands.py

--- Clone/commands.py
+++ Clone/commands.py
@@ -307,3 +307,36 @@ async def pm_send(client, message):
         val = 1
     if val != 1:
         await client.send_message(chat_id=message.from_user.id, text="Message Send To User Successfully")
+
+#Send msg to admin
+@Client.on_message(filters.command('admin'))
+async def admin_send(client, message):
+    try:
+        # the user ID
+        me = await client.get_me()
+        clone_owner = await db.get_bot(me.id)
+        CLONE_ADMIN = int(owner["user_id"])
+        # Ask for the message to send
+        PM_MESSAGE = await client.ask(chat_id=message.from_user.id, text="Now Send Me The Message To Be Sent")
+        
+        userid = message.from_user.id
+        DEFAULT_TXT = """YOU HAVE A MESSAGE FROM USER ID <code>{user}</code>\n\n"""
+        
+        # Format the user ID into the default text
+        USER_TXT = DEFAULT_TXT.format(user=userid)
+        
+        # Combine the default text with the user's message
+        pm_text = USER_TXT + PM_MESSAGE.text
+        
+        # Send the message to the admin
+        await client.send_message(chat_id=CLONE_ADMIN, text=pm_text)
+        fs = 0
+
+    except ValueError:
+        await client.send_message(chat_id=message.from_user.id, text="Something wrong Try again later")
+        fs = 1
+    except Exception as e:
+        await client.send_message(chat_id=message.from_user.id, text=f"An error occurred: {str(e)}")
+        fs = 1
+    if fs != 1:
+        await client.send_message(chat_id=message.from_user.id, text="Message Send to Admin")
